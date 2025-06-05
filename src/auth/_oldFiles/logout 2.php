d94082e2e9e97803f14c54e0c1b2de29fe44f9e1
Fetrixx <medinaelias69@gmail.com>
2025-06-05 10:16:14 -0300

    deleted _oldFiles

--- src/auth/_oldFiles/logout 2.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/session.php';
-
-session_start();
-destroySession();
-header('Location: /projectFoodTruckManager/public/login.php');
-exit;
--- src/auth/_oldFiles/logout old.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/session.php';
-
-destroySession();
-header('Location: /projectFoodTruckManager/public/login.php');
-exit;
